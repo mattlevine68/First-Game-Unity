33d619d1577b5e08a6e502c0dedf8445e4924a67
mattlevine68 <51725771+mattlevine68@users.noreply.github.com>
2020-06-24 23:11:34 -0400

    Uploaded Intial

---
+++ PracticeUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void EndGame()
+    {
+      if (!gameHasEnded)
+      {
+        gameHasEnded = true;
+        Invoke("Restart", restartDelay);
+      }
+    }
+
+    void Restart()
+    {
+      //First one can choose any level
+      //Second restarts level
+      //SceneManager.LoadScene("Level1");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel()
+    {
+      completeLevelUI.SetActive(true);
+    }
+}
---
+++ PracticeUnity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Use other components properties
+    public Rigidbody rb;
+
+    public float fowardForce = 2000f;
+    public float sideForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    //When using physics use FixedUpdate instead of Update
+    void FixedUpdate()
+    {
+      //Adds foward force
+      //x y z force mode
+      rb.AddForce(0,0,fowardForce * Time.deltaTime);
+
+      if (Input.GetKey("d") || Input.GetKey("right"))
+      {
+        rb.AddForce(sideForce*Time.deltaTime, 0,0, ForceMode.VelocityChange);
+      }
+      if (Input.GetKey("a") || Input.GetKey("left"))
+      {
+        rb.AddForce(-sideForce*Time.deltaTime, 0,0,ForceMode.VelocityChange);
+      }
+      if (rb.position.y < -1)
+      {
+        FindObjectOfType<GameManager>().EndGame();
+      }
+    }
+}
